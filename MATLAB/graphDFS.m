c3b6b313a8fba062b07c1fc5264476fa453646ba
Shantanu Tyagi <shantanutyagi67@gmail.com>
2020-06-19 23:34:40 +0530

    data analysis in matlab

---
+++ MATLAB/graphDFS.m
@@ -0,0 +1,17 @@
+clear all;
+close all;
+fid = fopen('C:\Users\geekSA67\code\SnakeAI\MATLAB\ScoreData.txt','rt');
+fx1000='%f';
+C = cell2mat(textscan(fid, fx1000,'Delimiter',';'));
+fclose(fid);
+
+figure(1)
+meanC = mean(C',2)
+plot(ones(length(C),1).*meanC,'b')
+hold on;
+plot(C(:,1),'r')
+ylabel('Score');
+xlabel('Tests');
+legend('Average','Score');
+savefig('C:\Users\geekSA67\code\SnakeAI\MATLAB\DFS.fig')
+saveas(gcf,'C:\Users\geekSA67\code\SnakeAI\Results\DFS.png')
